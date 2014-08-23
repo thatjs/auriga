12c35b341885834574d8a0cedc4cd634364e68d8
thatjs <thatjsapp@gmail.com>
2014-08-22 17:32:51 -0700

    Added apiService ng service object.

--- src/main/webapp/js/thatjs/services/services.js
+++ src/main/webapp/js/thatjs/services/services.js
@@ -1,45 +1,49 @@
-// router
-var thatjs = angular.module('thatjs', [
-    'ngRoute',
-    'pageControllers'
-]);
+/*globals angular */
+// apiService
+// inject dependency into module, then call the service object method
+//
+// Usage:
+// angular.module('thatjs.widget', ['thatjs.apiService'])
+//   .controller('widget', function ($scope, $http, apiService) {
+//     ...
+//     apiService.getData(itemId).then(function (promise) {
+//       $scope.myVar = promise.results;
+//     });
+//
+// Notes:
+//   $http methods return promise objects
 
+angular.module('thatjs.apiService', [])
+    .service('apiService', ['$http', function ($http) {
+        return {
+            baseUrl: '/api/Navigation',
+            getNavigation: function () {
+                var promise = $http.get(this.baseUrl)
+                    .success(function (data, status, headers, config) {
+                        return data;
+                    })
+                    .error(function (data, status, headers, config) {
+                        return {
+                            "status": false
+                        };
+                    });
 
-
-
-// services
-var services = angular.module('thatjs.services', ['ngResource']);
-
-services.factory('Recipe', ['$resource',
-    function ($resource) {
-        return $resource('/recipes/:id', { id: '@id' });
-    }]);
-
-services.factory('MultiRecipeLoader', ['Recipe', '$q',
-    function (Recipe, $q) {
-        return function () {
-            var delay = $q.defer();
-            Recipe.query(function (recipes) {
-                delay.resolve(recipes);
+                return promise;
             },
-                function () {
-                    delay.reject('Unable to fetch recipes');
-                });
-            return delay.promise;
-        };
-    }]);
 
-services.factory('RecipeLoader', ['Recipe', '$route', '$q',
-    function (Recipe, $route, $q) {
-        return function () {
-            var delay = $q.defer();
-            // ... stopped here ...
-            Recipe.get(function (recipes) {
-                delay.resolve(recipes);
-            },
-                function () {
-                    delay.reject('Unable to fetch recipes');
-                });
-            return delay.promise;
+            getCommand: function (commandId) {
+                // /api/Command(000267a3-5c30-40aa-80da-6773466d3a34)
+                var promise = $http.get('/api/Command(' + commandId + ')')
+                    .success(function (data, status, headers, config) {
+                        return data;
+                    })
+                    .error(function (data, status, headers, config) {
+                        return {
+                            "status": false
+                        };
+                    });
+
+                return promise;
+            }
         };
     }]);
