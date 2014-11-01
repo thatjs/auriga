f07bcfc392454f664660296ad9c0fa77ca82b885
thatjs <thatjsapp@gmail.com>
2014-11-01 18:24:05 +0800

    deleted files from local, updated header, layout.less

--- src/main/webapp/js/thatjs/main/app.js
+++
@@ -1,6 +0,0 @@
-/*globals window, console */
-// dev link /js/thatjs/main/app.js
-// prod /v1000/js/out/that.js - optimized, minified and compressed
-
-// declare module name and dependencies
-angular.module('thatjs', ['quickNotes']);
