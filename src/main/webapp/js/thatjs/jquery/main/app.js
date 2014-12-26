821331db33761ac83b68f8a5c3370efcffb5e394
thatjs <thatjsapp@gmail.com>
2014-12-25 18:41:06 -0800

    added media query inspection test code in app.js

--- src/main/webapp/js/thatjs/jquery/main/app.js
+++ src/main/webapp/js/thatjs/jquery/main/app.js
@@ -1,1 +1,14 @@
-$('#content').html('injected jquery content 2');
+$(document).ready(function () {
+
+    // can conditionally add event listeners, to mql objects that will fire when
+    // media query changes (orientation: profile) for ex.
+    var mql = window.matchMedia("only screen and (max-width: 760px)");
+
+    if (mql.matches) {
+        //Conditional script here
+        $('#content').html('injected jquery max-width < 760px');
+    } else {
+        $('#content').html('injected jquery max-width > 760px');
+    }
+
+});
