fb1c0f3a8bacf2ed50e764f17a6c79e5098ef9e7
thatjs <thatjsapp@gmail.com>
2015-01-01 12:10:08 -0800

    added viewport meta tag.

--- src/main/webapp/js/thatjs/jquery/main/app.js
+++ src/main/webapp/js/thatjs/jquery/main/app.js
@@ -1,10 +1,19 @@
 $(document).ready(function () {
 
+    // declare namespace
+    var th = {};
+
+    };
     // can conditionally add event listeners, to mql objects that will fire when
     // media query changes (orientation: profile) for ex.
-    var mql = window.matchMedia("only screen and (max-width: 760px)");
 
-    if (mql.matches) {
+    // should be a registration service pub/sub that will fire an event based on
+    // changes to the browser (width, orientation ... )
+
+    // using media queries, we dynamically load the
+    mqLayout = window.matchMedia("only screen and (max-width: 760px)");
+
+    if (mqLayout.matches) {
         //Conditional script here
         $('#content').html('injected jquery max-width < 760px');
     } else {
