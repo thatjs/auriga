8a405bc68c127a322ca5273e240a21807a5494f0
thatjs <thatjsapp@gmail.com>
2015-02-15 12:36:21 -0800

    started on gulp-concat, uglify to build js/out files.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     jslint = require('gulp-jslint'),
     karma = require('gulp-karma'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
 
     path = require('path');
 
@@ -19,50 +21,67 @@ var config = require('./config/gulp');
 
 
 
-function runKarma(configFilePath, options, callback) {
+//function runKarma(configFilePath, options, callback) {
+//
+//    configFilePath = path.resolve(configFilePath);
+//
+//    var server = karma.server,
+//        log = gUtil.log,
+//        colors = gUtil.colors,
+//        config = karmaParseConfig(configFilePath, {});
+//
+//    Object.keys(options).forEach(function (key) {
+//        config[key] = options[key];
+//    });
+//
+//    server.start(config, function (exitCode) {
+//        log('Karma has exited with ' + colors.red(exitCode));
+//        callback();
+//        process.exit(exitCode);
+//    });
+//}
 
-    configFilePath = path.resolve(configFilePath);
 
-    var server = karma.server,
-        log = gUtil.log,
-        colors = gUtil.colors,
-        config = karmaParseConfig(configFilePath, {});
-
-    Object.keys(options).forEach(function (key) {
-        config[key] = options[key];
-    });
-
-    server.start(config, function (exitCode) {
-        log('Karma has exited with ' + colors.red(exitCode));
-        callback();
-        process.exit(exitCode);
-    });
-}
+// not working yet
+//gulp.task('install', function () {
+//    return gulp.src(jsPath + 'angular/' + jsSrc)
+//        .pipe(concat('angular/that.js'))
+//        .pipe(gulp.dest(jsOutPath))
+//        .pipe(uglify())
+//        .pipe(rename({ extname: '.min.js'}))
+//        .pipe(gulp.dest(jsOutPath));
+//
+//});
 
 gulp.task('clean', function () {
-    return gulp
-        .src(['target', cssPath, jsPath], { read: false})
+    return gulp.src(['target', cssPath, jsPath], { read: false})
         .pipe(clean());
 });
 
 gulp.task('lint', function () {
-    return gulp
-        .src(jsSrc)
+    return gulp.src(jsSrc)
         .pipe(jslint(config.jslint))
 });
 
-gulp.task('test', function (callback) {
-    runKarma('karma.conf.js', {
-        autoWatch: false,
-        singleRun: true
-    }, callback);
+gulp.task('test', function () {
+    return gulp
+        .src([])  // use config file globs
+        .pipe(karma({
+            configFile: config.karma.configFile,
+            action: 'run'  // run tests once, then exit
+        }))
+        .on('error', function (err) {
+            throw err;  // make sure gulp exits non-zero on error
+        });
+
 });
 
 gulp.task('tdd', function (callback) {
-    runKarma('karma.conf.js', {
-        autoWatch: true,
-        singleRun: false
-    }, callback);
+    gulp.src([])
+        .pipe(karma({
+            configFile: config.karma.configFile,
+            action: 'watch'  // run tests, watch files and run when files change
+        }));
 });
 
 //gulp.task('build', ['clean'], function () {
